5e27ba0793c4e0af2604d3d949dda4c8ab21a07b
Rawthul <ozgurgokd@gmail.com>
2022-07-02 22:57:36 +0300

    Setup React Icons & Update Searchbar

--- restaurant-menu-frontend/src/components/Navbar/index.jsx
+++ restaurant-menu-frontend/src/components/Navbar/index.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
 	const linkClasses = 'text-lg font-medium text-[#353535] hover:opacity-70';
 	return (
 		<nav className='flex gap-12'>
-			<Link to='/' className={linkClasses}>Home</Link>
-			<Link to='' className={linkClasses}>Menu</Link>
-			<Link to='' className={linkClasses}>Popular</Link>
+			<Link to='/' className={linkClasses}>HOME</Link>
+			<Link to='' className={linkClasses}>MENU</Link>
+			<Link to='' className={linkClasses}>CONTACT</Link>
 		</nav>
 	);
 };
