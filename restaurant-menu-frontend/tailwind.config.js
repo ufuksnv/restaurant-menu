c785397e98fedebab57db4980588f2f424751b96
Rawthul <ozgurgokd@gmail.com>
2022-07-06 21:38:07 +0300

    Add Custom Colors to Tailwind Config

--- restaurant-menu-frontend/tailwind.config.js
+++ restaurant-menu-frontend/tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				accent: '#fa6235',
+			},
+			textColor: {
+				light: {
+					primary: 'rgb(255 255 255 / 1)',
+					secondary: 'rgb(255 255 255 / .7)',
+					disabled: 'rgb(255 255 255 / .5)',
+					dividers: 'rgb(255 255 255 / .12)',
+				},
+				dark: {
+					primary: 'rgb(0 0 0 / .87)',
+					secondary: 'rgb(0 0 0 / .54)',
+					disabled: 'rgb(0 0 0 / .38)',
+					dividers: 'rgb(0 0 0 / .12)',
+				},
+			},
+			backgroundColor: {
+				light: {
+					50: '#ffffff',
+					100: '#fafafa',
+					200: '#f5f5f5',
+				},
+				dark: {
+					50: '#36454F',
+					100: '#495D6A',
+					200: '#5B7586',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
