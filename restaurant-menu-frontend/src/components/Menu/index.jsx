24f6910d26a13b523b8327a8546ceaab8847df32
Rawthul <ozgurgokd@gmail.com>
2022-07-09 20:52:42 +0300

    Add Color Transition

--- restaurant-menu-frontend/src/components/Menu/index.jsx
+++ restaurant-menu-frontend/src/components/Menu/index.jsx
@@ -7,11 +7,11 @@ import ColorModeMenu from '../ColorModeMenu';
 
 const Menu = ({ isOpen, onClose }) => {
 	return (
-		<div className={`${!isOpen ? 'hidden lg:block' : 'block'} fixed bg-[rgba(0,0,0,0.5)] inset-0 z-50 lg:static lg:bg-transparent lg:z-auto`}>
-			<div className='max-w-full p-4 lg:p-0 flex flex-col absolute inset-y-0 right-0 gap-6 lg:gap-12 bg-light-50 dark:bg-dark-50 lg:flex-row lg:items-center lg:static'>
+		<div className={`${!isOpen ? 'hidden lg:block' : 'block'} fixed lg:static bg-[rgba(0,0,0,0.5)] lg:bg-transparent transition-colors inset-0 z-50 lg:z-auto`}>
+			<div className='max-w-full p-4 lg:p-0 flex flex-col absolute inset-y-0 right-0 gap-6 lg:gap-12 bg-light-50 dark:bg-dark-50 transition-colors lg:flex-row lg:items-center lg:static'>
         <IconButton
           isHidden={!isOpen}
-					className='lg:hidden h-9 mr-2 ml-auto text-4xl mt-[0.875rem] sm:mt-[1.375rem] md:mt-[1.875rem]'
+					className='lg:hidden h-9 mr-2 ml-auto text-4xl mt-[0.875rem] sm:mt-[1.375rem] md:mt-[1.875rem] text-dark-primary dark:text-light-primary transition-colors'
           icon={RiCloseFill}
 					onClick={onClose}
 				/>
--- restaurant-menu-frontend/src/components/Navbar/index.jsx
+++ restaurant-menu-frontend/src/components/Navbar/index.jsx
@@ -5,27 +5,51 @@ import { CgMenuGridO } from 'react-icons/cg';
 import { MdContactSupport } from 'react-icons/md';
 
 const Navbar = ({ className = '' }) => {
-	const classes = {
-		nav: `px-4 flex flex-col lg:flex-row bg-white dark:bg-dark-100 lg:bg-transparent lg:dark:bg-transparent rounded-lg shadow-md lg:shadow-none lg:gap-12 ${className}`.trim(),
-		link: 'text-lg font-medium hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent transition-colors flex lg:block items-center gap-4 py-3 lg:p-0 border-b dark:border-charcoal-200 lg:border-none last:border-none',
-		icon: 'h-9 grid lg:hidden place-items-center aspect-square',
-	};
 	return (
-		<nav className={classes.nav}>
-			<NavLink to='/' className={({ isActive }) => `${classes.link} ${isActive ? 'text-dark-primary dark:text-light-primary' : 'text-dark-disabled dark:text-light-disabled'}`}>
-				<span className={classes.icon}>
+		<nav
+			className={`px-4 flex flex-col lg:flex-row lg:gap-12 rounded-lg shadow-md lg:shadow-none bg-white dark:bg-dark-100 lg:bg-transparent lg:dark:bg-transparent transition-colors ${className}`.trim()}
+		>
+			<NavLink
+				to='/'
+				className={({ isActive }) =>
+					`flex lg:block items-center gap-4 py-3 lg:p-0 font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent transition-colors dark:border-charcoal-200 border-b lg:border-none last:border-none ${
+						isActive
+							? 'text-dark-primary dark:text-light-primary'
+							: 'text-dark-disabled dark:text-light-disabled'
+					}`
+				}
+			>
+				<span className='h-9 grid lg:hidden place-items-center aspect-square'>
 					<AiFillHome className='text-3xl' />
 				</span>
 				HOME
 			</NavLink>
-			<NavLink to='/menu' className={({ isActive }) => `${classes.link} ${isActive ? 'text-dark-primary dark:text-light-primary' : 'text-dark-disabled dark:text-light-disabled'}`}>
-				<span className={classes.icon}>
+			<NavLink
+				to='/menu'
+				className={({ isActive }) =>
+					`flex lg:block items-center gap-4 py-3 lg:p-0 font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent transition-colors dark:border-charcoal-200 border-b lg:border-none last:border-none ${
+						isActive
+							? 'text-dark-primary dark:text-light-primary'
+							: 'text-dark-disabled dark:text-light-disabled'
+					}`
+				}
+			>
+				<span className='h-9 grid lg:hidden place-items-center aspect-square'>
 					<CgMenuGridO className='text-3xl' />
 				</span>
 				MENU
 			</NavLink>
-			<NavLink to='/contact' className={({ isActive }) => `${classes.link} ${isActive ? 'text-dark-primary dark:text-light-primary' : 'text-dark-disabled dark:text-light-disabled'}`}>
-				<span className={classes.icon}>
+			<NavLink
+				to='/contact'
+				className={({ isActive }) =>
+					`flex lg:block items-center gap-4 py-3 lg:p-0 font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent transition-colors dark:border-charcoal-200 border-b lg:border-none last:border-none ${
+						isActive
+							? 'text-dark-primary dark:text-light-primary'
+							: 'text-dark-disabled dark:text-light-disabled'
+					}`
+				}
+			>
+				<span className='h-9 grid lg:hidden place-items-center aspect-square'>
 					<MdContactSupport className='text-3xl' />
 				</span>
 				CONTACT
