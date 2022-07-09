93868118ebd9432af344786e0aa48489c968b9df
Rawthul <ozgurgokd@gmail.com>
2022-07-09 23:52:49 +0300

    Update Navbar

--- restaurant-menu-frontend/src/components/Navbar/index.jsx
+++ restaurant-menu-frontend/src/components/Navbar/index.jsx
@@ -7,51 +7,45 @@ import { MdContactSupport } from 'react-icons/md';
 const Navbar = ({ className = '' }) => {
 	return (
 		<nav
-			className={`px-4 flex flex-col lg:flex-row lg:gap-12 rounded-lg shadow-md lg:shadow-none bg-white dark:bg-dark-100 lg:bg-transparent lg:dark:bg-transparent transition-colors ${className}`.trim()}
+			className={`lg:px-4 flex flex-col lg:flex-row lg:gap-12 rounded-lg shadow-md lg:shadow-none bg-white dark:bg-dark-100 lg:bg-transparent lg:dark:bg-transparent transition-colors ${className}`.trim()}
 		>
 			<NavLink
 				to='/'
 				className={({ isActive }) =>
-					`flex lg:block items-center gap-4 py-3 lg:p-0 font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent transition-colors dark:border-charcoal-200 border-b lg:border-none last:border-none ${
+					` flex lg:block items-center gap-4 px-4 py-3 lg:p-0 rounded-t-lg lg:rounded-none font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent hover:bg-light-200 lg:hover:bg-transparent dark:hover:bg-dark-200 lg:dark:hover:bg-transparent transition-colors ${
 						isActive
 							? 'text-dark-primary dark:text-light-primary'
 							: 'text-dark-disabled dark:text-light-disabled'
 					}`
 				}
 			>
-				<span className='h-9 grid lg:hidden place-items-center aspect-square'>
-					<AiFillHome className='text-3xl' />
-				</span>
+				<AiFillHome className='lg:hidden text-3xl' />
 				HOME
 			</NavLink>
 			<NavLink
 				to='/menu'
 				className={({ isActive }) =>
-					`flex lg:block items-center gap-4 py-3 lg:p-0 font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent transition-colors dark:border-charcoal-200 border-b lg:border-none last:border-none ${
+					`flex lg:block items-center gap-4 px-4 py-3 lg:p-0 font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent hover:bg-light-200 lg:hover:bg-transparent dark:hover:bg-dark-200 lg:dark:hover:bg-transparent transition-colors ${
 						isActive
 							? 'text-dark-primary dark:text-light-primary'
 							: 'text-dark-disabled dark:text-light-disabled'
 					}`
 				}
 			>
-				<span className='h-9 grid lg:hidden place-items-center aspect-square'>
-					<CgMenuGridO className='text-3xl' />
-				</span>
+				<CgMenuGridO className='lg:hidden text-3xl' />
 				MENU
 			</NavLink>
 			<NavLink
 				to='/contact'
 				className={({ isActive }) =>
-					`flex lg:block items-center gap-4 py-3 lg:p-0 font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent transition-colors dark:border-charcoal-200 border-b lg:border-none last:border-none ${
+					`flex lg:block items-center gap-4 px-4 py-3 lg:p-0 rounded-b-lg lg:rounded-none font-medium text-lg hover:text-accent dark:hover:text-accent focus-visible:text-accent dark:focus-visible:text-accent hover:bg-light-200 lg:hover:bg-transparent dark:hover:bg-dark-200 lg:dark:hover:bg-transparent transition-colors ${
 						isActive
 							? 'text-dark-primary dark:text-light-primary'
 							: 'text-dark-disabled dark:text-light-disabled'
 					}`
 				}
 			>
-				<span className='h-9 grid lg:hidden place-items-center aspect-square'>
-					<MdContactSupport className='text-3xl' />
-				</span>
+				<MdContactSupport className='lg:hidden text-3xl' />
 				CONTACT
 			</NavLink>
 		</nav>
